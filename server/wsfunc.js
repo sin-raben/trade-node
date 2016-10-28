c1b4f6c17a0dcdd7174fa5cc364f5c939dad5c0f
sin-raben <zsg878@gmail.com>
2016-10-28 13:40:15 +0300

    mtime - integer

--- server/wsfunc.js
+++ server/wsfunc.js
@@ -126,42 +126,49 @@ var wsfunc = {
                 var pr = [];
                 var nm = [];
                 if (obj.items === "all") {
-                    pr.push(db.query("SELECT *, extract(epoch from i_mtime)::integer as i_mtime_i from items", {}));
+                    pr.push(db.query("SELECT i_id,i_name,i_prn,i_service,int_id,i_active, extract(epoch from i_mtime)::integer as i_mtime from items", {}));
                     nm.push("items");
                 }
                 if (obj.itemGroupTypes === "all") {
-                    pr.push(db.query("SELECT *, extract(epoch from igt_mtime)::integer as igt_mtime_i from item_Group_Types", {}));
+                    pr.push(db.query("SELECT igt_id, igt_name, igt_agent, igt_active, extract(epoch from igt_mtime)::integer as igt_mtime from item_Group_Types;", {}));
                     nm.push("itemGroupTypes");
                 }
                 if (obj.itemGroups === "all") {
-                    pr.push(db.query("SELECT *, extract(epoch from ig_mtime)::integer as ig_mtime_i from item_Groups", {}));
+                    pr.push(db.query("SELECT ig_id,igt_id,ig_value,ig_active, extract(epoch from ig_mtime)::integer as ig_mtime from item_Groups", {}));
                     nm.push("itemGroups");
                 }
                 if (obj.linkItemGroups === "all") {
-                    pr.push(db.query("SELECT *, extract(epoch from lig_mtime)::integer as lig_mtime_i from link_Item_Group", {}));
+                    pr.push(db.query("SELECT lig_id,i_id,ig_id,igt_id,lig_active, extract(epoch from lig_mtime)::integer as lig_mtime from link_Item_Group", {}));
                     nm.push("linkItemGroups");
                 }
                 if (obj.itemUnitTypes === "all") {
-                    pr.push(db.query("SELECT *, extract(epoch from iut_mtime)::integer as iut_mtime_i from item_Unit_Types", {}));
+                    pr.push(db.query("SELECT iut_id,iut_name,imt_id,iut_okei,iut_active, extract(epoch from iut_mtime)::integer as iut_mtime from item_Unit_Types", {}));
                     nm.push("itemUnitTypes");
                 }
                 if (obj.itemUnits === "all") {
-                    pr.push(db.query("SELECT *, extract(epoch from iu_mtime)::integer as iu_mtime_i from item_Units", {}));
+                    pr.push(db.query("SELECT iu_id,i_id, iut_id, iu_ean,iu_krat, iu_num, iu_denum,iu_gros, iu_net, iu_length, iu_width, iu_height, iu_area, iu_volume, "+
+                    "iu_agent, iu_base, iu_main, iu_active, extract(epoch from iu_mtime)::integer as iu_mtime from item_Units", {}));
                     nm.push("itemUnits");
                 }
                 if (obj.itemsSearch === "all") {
-                    pr.push(db.query("SELECT i.i_id, concat_ws(' ', ig1.ig_value, ig2.ig_value, ig3.ig_value, i_name) as value FROM items i "+
-                    "LEFT JOIN link_item_group lig1 ON (i.i_id = lig1.i_id) AND (lig1.igt_id=1) "+
-                    "LEFT JOIN item_groups ig1 ON lig1.ig_id = ig1.ig_id "+
-                    "LEFT JOIN link_item_group lig2 ON (i.i_id = lig2.i_id) AND (lig2.igt_id=2) "+
-                    "LEFT JOIN item_groups ig2 ON lig2.ig_id = ig2.ig_id "+
-                    "LEFT JOIN link_item_group lig3 ON (i.i_id = lig3.i_id) AND (lig3.igt_id=3) "+
-                    "LEFT JOIN item_groups ig3 ON lig3.ig_id = ig3.ig_id;", {}));
+                    pr.push(db.query("SELECT i.i_id, concat_ws(' ', ig1.ig_value, ig2.ig_value, ig3.ig_value, i_name) as value FROM items i " +
+                        "LEFT JOIN link_item_group lig1 ON (i.i_id = lig1.i_id) AND (lig1.igt_id=1) " +
+                        "LEFT JOIN item_groups ig1 ON lig1.ig_id = ig1.ig_id " +
+                        "LEFT JOIN link_item_group lig2 ON (i.i_id = lig2.i_id) AND (lig2.igt_id=2) " +
+                        "LEFT JOIN item_groups ig2 ON lig2.ig_id = ig2.ig_id " +
+                        "LEFT JOIN link_item_group lig3 ON (i.i_id = lig3.i_id) AND (lig3.igt_id=3) " +
+                        "LEFT JOIN item_groups ig3 ON lig3.ig_id = ig3.ig_id;", {}));
                     nm.push("itemsSearch");
                 }
                 Promise.all(pr).then((value) => {
                     for (let i = 0; i < nm.length; i++) {
-                        tov[nm[i]] = value[i];
+                        let arr = value[i];
+                        arr.forEach(elem => {
+                            for (let prop in elem) {
+                                if (elem[prop] === null) delete elem[prop];
+                            }
+                        });
+                        tov[nm[i]] = arr;
                     }
                     console.log("tov");
                     resolve(tov);
@@ -186,75 +193,77 @@ var wsfunc = {
             if (obj.items) {
                 obj.items.forEach(elem => {
                     db.query("INSERT INTO items (i_exid, i_name, i_prn, i_info, i_img, i_service, int_id)" +
-                        " VALUES (${i_exid}, ${i_name}, ${i_prn}, ${i_info}, ${i_img}, ${i_service}, ${int_id});",elem)
-                        .then((value) => {
+                        " VALUES (${i_exid}, ${i_name}, ${i_prn}, ${i_info}, ${i_img}, ${i_service}, ${int_id});",
+                    elem).then((value) => {
                         //console.log('value', value);
                     }, (err) => {
-                        console.log('err',elem,'\n\n\n', err);
+                        console.log('err', elem, '\n\n\n', err);
                     });
                 });
             }
             if (obj.itemsGroupType) {
                 obj.itemsGroupType.forEach(elem => {
                     db.query("INSERT INTO item_Group_Types (igt_exid, igt_agent, igt_name)" +
-                        " VALUES (${igt_exid}, ${igt_agent}, ${igt_name});",elem)
-                        .then((value) => {
+                        " VALUES (${igt_exid}, ${igt_agent}, ${igt_name});",
+                    elem).then((value) => {
                         //добавлено
                     }, (err) => {
-                        console.log('err',elem,'\n\n\n', err);
+                        console.log('err', elem, '\n\n\n', err);
                     });
                 });
             }
             if (obj.itemsGroup) {
                 obj.itemsGroup.forEach(elem => {
-                    db.query("INSERT INTO item_groups (igt_id, ig_exid, ig_value) SELECT igt.igt_id, t.ig_exid, t.ig_value FROM item_group_types AS igt "+
-                        "CROSS JOIN (VALUES (${ig_exid}, ${ig_value})) AS t (ig_exid, ig_value) WHERE igt.igt_exid = ${igt_exid};",elem)
-                        .then((value) => {
+                    db.query("INSERT INTO item_groups (igt_id, ig_exid, ig_value) SELECT igt.igt_id, t.ig_exid, t.ig_value FROM item_group_types AS igt " +
+                        "CROSS JOIN (VALUES (${ig_exid}, ${ig_value})) AS t (ig_exid, ig_value) WHERE igt.igt_exid = ${igt_exid};",
+                    elem).then((value) => {
                         //добавлено
                     }, (err) => {
-                        console.log('err',elem,'\n\n\n', err);
+                        console.log('err', elem, '\n\n\n', err);
                     });
                 });
             }
             if (obj.linkItemGroup) {
                 obj.linkItemGroup.forEach(elem => {
-                    db.query("INSERT INTO link_Item_Group (i_id, igt_id, ig_id) SELECT i_id, igt_id, ig_id FROM items as i "+
-                    "CROSS JOIN (SELECT ig.igt_id, ig_id FROM item_groups AS ig JOIN item_group_types AS igt ON igt.igt_id=ig.igt_id "+
-                    "WHERE igt.igt_exid = ${igt_exid} AND ig.ig_exid = ${ig_exid}) as ig WHERE i.i_exid = ${i_exid};", elem)
-                    .then((value) => {
+                    db.query("INSERT INTO link_Item_Group (i_id, igt_id, ig_id) SELECT i_id, igt_id, ig_id FROM items as i " +
+                        "CROSS JOIN (SELECT ig.igt_id, ig_id FROM item_groups AS ig JOIN item_group_types AS igt ON igt.igt_id=ig.igt_id " +
+                        "WHERE igt.igt_exid = ${igt_exid} AND ig.ig_exid = ${ig_exid}) as ig WHERE i.i_exid = ${i_exid};",
+                    elem).then((value) => {
                         //добавлено
                     }, (err) => {
-                        console.log('err',elem,'\n\n\n', err);
+                        console.log('err', elem, '\n\n\n', err);
                     });
                 });
             }
             if (obj.itemsUnitType) {
                 obj.itemsUnitType.forEach(elem => {
-                    db.query("INSERT INTO item_Unit_Types (iut_exid, iut_name, imt_id, iut_okei) "+
-                             "VALUES (${iut_exid}, ${iut_name}, ${imt_id}, ${iut_okei});", elem)
-                    .then((value) => {
+                    db.query("INSERT INTO item_Unit_Types (iut_exid, iut_name, imt_id, iut_okei) " +
+                        "VALUES (${iut_exid}, ${iut_name}, ${imt_id}, ${iut_okei});",
+                    elem).then((value) => {
                         //добавлено
                     }, (err) => {
-                        console.log('err',elem,'\n\n\n', err);
+                        console.log('err', elem, '\n\n\n', err);
                     });
                 });
             }
             if (obj.itemsUnit) {
 
                 obj.itemsUnit.forEach(elem => {
-                    db.query("INSERT INTO item_units "+
-                    "(i_id, iut_id, iu_ean, iu_krat, iu_num, iu_denum, iu_gros, iu_net, "+
-                    "iu_length, iu_width, iu_height, iu_area, iu_volume, iu_agent, iu_base, iu_main) "+
-                    "SELECT i.i_id, iut.iut_id, iu_ean, iu_krat, iu_num, iu_denum, iu_gros, iu_net, "+
-                    "       iu_length, iu_width, iu_height, iu_area, iu_volume, iu_agent, iu_base, iu_main FROM items as i "+
-                    "CROSS JOIN item_unit_types as iut CROSS JOIN (VALUES(${iu_ean}, ${iu_krat}, ${iu_num}, ${iu_denum}, ${iu_gros}, ${iu_net}, "+
-                    "${iu_length}, ${iu_width}, ${iu_height}, ${iu_area}, ${iu_volume}, ${iu_agent}, ${iu_base}, ${iu_main})) AS t "+
-                    "(iu_ean, iu_krat, iu_num, iu_denum, iu_gros, iu_net, iu_length, iu_width, iu_height, iu_area, iu_volume, iu_agent, iu_base, iu_main) "+
-                    "WHERE i.i_exid = ${i_exid} AND iut.iut_exid = ${iut_exid};", elem)
-                    .then((value) => {
+                    db.query("INSERT INTO item_units " +
+                        "(i_id, iut_id, iu_ean, iu_krat, iu_num, iu_denum, iu_gros, iu_net, " +
+                        "iu_length, iu_width, iu_height, iu_area, iu_volume, iu_agent, iu_base, iu_main) " +
+                        "SELECT i.i_id, iut.iut_id, iu_ean, iu_krat, iu_num, iu_denum, iu_gros, iu_net, " +
+                        "       iu_length, iu_width, iu_height, iu_area, iu_volume, iu_agent, iu_base, iu_main FROM items as i " +
+                        "CROSS JOIN item_unit_types as iut " +
+                        "CROSS JOIN (VALUES(${iu_ean}, ${iu_krat}, ${iu_num}, ${iu_denum}, ${iu_gros}, ${iu_net}, " +
+                        "    ${iu_length}, ${iu_width}, ${iu_height}, ${iu_area}, ${iu_volume}, ${iu_agent}, ${iu_base}, ${iu_main})) " +
+                        "    AS t (iu_ean, iu_krat, iu_num, iu_denum, iu_gros, iu_net, " +
+                        "    iu_length, iu_width, iu_height, iu_area, iu_volume, iu_agent, iu_base, iu_main) " +
+                        "WHERE i.i_exid = ${i_exid} AND iut.iut_exid = ${iut_exid};",
+                    elem).then((value) => {
                         //добавлено
                     }, (err) => {
-                        console.log('err',elem,'\n\n\n', err);
+                        console.log('err', elem, '\n\n\n', err);
                     });
                 });
             }
@@ -270,16 +279,110 @@ var wsfunc = {
             if (obj.items) {
                 obj.items.forEach(elem => {
                     db.query("INSERT INTO items (i_exid, i_name, i_prn, i_info, i_img, i_service, int_id)" +
-                        " VALUES (${i_exid}, ${i_name}, ${i_prn}, ${i_info}, ${i_img}, ${i_service}, ${int_id});",elem)
-                        .then((value) => {
+                        " VALUES (${i_exid}, ${i_name}, ${i_prn}, ${i_info}, ${i_img}, ${i_service}, ${int_id});",
+                    elem).then((value) => {
                         //console.log('value', value);
                     }, (err) => {
-                        console.log('err',elem, err);
+                        console.log('err', elem, err);
                     });
                 });
             }
 
             resolve({"result": true});
+        });
+    },
+    setPrices: function(client, obj) {
+        return new Promise(function(resolve, reject) {
+            if (!(client.idToken)) {
+                resolve({"result": false});
+                return;
+            }
+            if (obj.pricelist) {
+                obj.pricelist.forEach(elem => {
+                    db.query("INSERT INTO pricelist (pl_exid, pl_name, pl_type, pl_active)" +
+                        " VALUES (${pl_exid}, ${pl_name}, ${pl_type}, ${pl_active});",
+                    elem).then((value) => {
+                        //console.log('value', value);
+                    }, (err) => {
+                        console.log('err', elem, err);
+                    });
+                });
+            }
+            if (obj.price) {
+
+                obj.price.forEach(elem => {
+
+                    db.query("INSERT INTO price (i_id, pl_id, p_date_b, p_date_e, p_cn) " +
+                        "SELECT  i.i_id, pl.pl_id, ${p_date_b}::timestamp as p_date_b, ${p_date_e}::timestamp as p_date_e, ${p_cn} as p_cn " +
+                        "FROM items i, pricelist pl WHERE i.i_exid = ${i_exid} AND pl_exid = ${pl_exid}",
+                    elem).then((value) => {
+
+                        // console.log('value', value);
+                    }, (err) => {
+                        console.log('err', elem, err);
+                    });
+                });
+            }
+            if (obj.pricelistLink) {
+                console.log('1', 1);
+                obj.pricelistLink.forEach(elem => {
+                    db.query("INSERT INTO pricelist_link (pl_parent, pl_child, pll_prior) " +
+                        " SELECT pl1.pl_id as pl_parent, pl2.pl_id as pl_child, ${pll_prior} as pll_prior FROM  pricelist pl1, pricelist pl2 " +
+                        " WHERE pl1.pl_exid = ${pl_exparent} AND pl2.pl_exid = ${pl_exchild}",
+                    elem).then((value) => {
+
+                        console.log('value', value);
+                    }, (err) => {
+                        console.log('err', elem, err);
+                    });
+                });
+            }
+
+            resolve({"result": true});
+        });
+    },
+    getPrices: function(client, obj) {
+        return new Promise(function(resolve, reject) {
+            if (!(client.idToken)) {
+                resolve({"result": false});
+                return;
+            }
+            try {
+                var price = {};
+                var pr = [];
+                var nm = [];
+                if (obj.pricelist === "all") {
+                    console.log(1);
+                    pr.push(db.query("SELECT pl_id,pl_name,pl_type,pl_active, extract(epoch from pl_mtime)*1000 as pl_mtime FROM pricelist", {}));
+                    nm.push("pricelist");
+                }
+                if (obj.price === "all") {
+                    console.log(2);
+                    pr.push(db.query("SELECT p_id,pl_id,i_id,extract(epoch from p_date_b)*1000 as p_date_b, extract(epoch from p_date_e)*1000 as p_date_e, "+
+                    "p_cn,p_active, extract(epoch from p_mtime)*1000 as p_mtime FROM price", {}));
+                    nm.push("price");
+                }
+                if (obj.pricelistLink === "all") {
+                    console.log(3);
+                    pr.push(db.query("SELECT * FROM pricelist_link", {}));
+                    nm.push("pricelistLink");
+                }
+                Promise.all(pr).then((value) => {
+                    for (let i = 0; i < nm.length; i++) {
+
+                        price[nm[i]] = value[i];
+                    }
+                    console.log("price");
+                    resolve(price);
+                }, (err) => {
+                    console.log('err all', err);
+                });
+                //var tov = require('../db/tov');
+
+            } catch (err) {
+                console.log("err", err);
+                reject(err);
+            }
         });
     },
     getCountragents: function(client, obj) {
@@ -294,27 +397,36 @@ var wsfunc = {
                 var pr = [];
                 var nm = [];
                 if (obj.countragents === "all") {
-                    pr.push(db.query("SELECT *, extract(epoch from ca_mtime)*1000 as ca_mtime_i from countragents", {}));
+                    pr.push(db.query("SELECT ca_id, ca_type, ca_opf, ca_head, ca_name, ca_prn, adr_id, ca_client, ca_supplier, ca_carrier, ca_active, "+
+                    "extract(epoch from ca_mtime)*1000 as ca_mtime from countragents", {}));
                     nm.push("countragents");
                 }
                 if (obj.deliveryPoints === "all") {
-                    pr.push(db.query("SELECT *, extract(epoch from dp_mtime)*1000 as dp_mtime_i from delivery_points", {}));
+                    pr.push(db.query("SELECT dp_id, dp_name, dp_prn, adr_id,dp_client, dp_supplier, dp_carrier, dp_active, "+
+                    "extract(epoch from dp_mtime)*1000 as dp_mtime from delivery_points", {}));
                     nm.push("deliveryPoints");
                 }
                 if (obj.address === "all") {
-                    pr.push(db.query("SELECT *, extract(epoch from adr_mtime)*1000 as adr_mtime_i from address", {}));
+                    pr.push(db.query("SELECT *, extract(epoch from adr_mtime)*1000 as adr_mtime from address", {}));
                     nm.push("address");
                 }
                 if (obj.linksCountragentDeliveryPoint === "all") {
-                    pr.push(db.query("SELECT *, extract(epoch from lcp_mtime)*1000 as lcp_mtime_i from links_countragent_delivery_point", {}));
+                    pr.push(db.query("SELECT lcp_id, lcp_org, ca_id,dp_id, lcp_active, "+
+                    "extract(epoch from lcp_mtime)*1000 as lcp_mtime from links_countragent_delivery_point", {}));
                     nm.push("linksCountragentDeliveryPoint");
                 }
 
                 Promise.all(pr).then((value) => {
                     for (let i = 0; i < nm.length; i++) {
-                        tov[nm[i]] = value[i];
+                        let arr = value[i];
+                        arr.forEach(elem => {
+                            for (let prop in elem) {
+                                if (elem[prop] === null) delete elem[prop];
+                            }
+                        });
+                        tov[nm[i]] = arr;
                     }
-                    console.log("tov");
+                    console.log("getCountragents");
                     resolve(tov);
                 }, (err) => {
                     console.log('err all', err);
